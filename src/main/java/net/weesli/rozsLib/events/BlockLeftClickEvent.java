e096506ed04b339245c60bcabb3de89ed0959e7c
Weesli <sefasam553498@gmail.com>
2024-08-16 13:57:16 +0300

    Added 2 custom events

---
+++ src/main/java/net/weesli/rozsLib/events/BlockLeftClickEvent.java
@@ -0,0 +1,49 @@
+package net.weesli.rozsLib.events;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class BlockLeftClickEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
+
+    private Player player;
+    private Block ClickedBlock;
+
+    public BlockLeftClickEvent(Player player, Block clickedBlock) {
+        this.player = player;
+        this.ClickedBlock = clickedBlock;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        cancelled = cancel;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Block getClickedBlock() {
+        return ClickedBlock;
+    }
+}
--- src/main/java/net/weesli/rozsLib/events/LibListener.java
+++ src/main/java/net/weesli/rozsLib/events/LibListener.java
@@ -1,12 +1,14 @@
 package net.weesli.rozsLib.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class LibListener implements Listener {
 
@@ -31,6 +33,26 @@ public class LibListener implements Listener {
             Bukkit.getPluginManager().callEvent(playerDamageByEntityEvent);
             if (playerDamageByEntityEvent.isCancelled()){
                 e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInteractPlayer(PlayerInteractEvent e){
+        Player player = e.getPlayer();
+        Block block = e.getClickedBlock();
+        if (block == null){ return;}
+        if (e.getAction().isLeftClick()){
+            BlockLeftClickEvent event = new BlockLeftClickEvent(player,block);
+            Bukkit.getPluginManager().callEvent(event);
+            if (e.isCancelled()){
+                event.setCancelled(true);
+            }
+        } else if (e.getAction().isRightClick()) {
+            BlockRightClickEvent event = new BlockRightClickEvent(player, block);
+            Bukkit.getPluginManager().callEvent(event);
+            if (e.isCancelled()){
+                event.setCancelled(true);
             }
         }
     }
