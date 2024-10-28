4e67a8c237cbb7b9ae9d7f8f5db5739fce8dfc42
Weesli <78960827+Weesli@users.noreply.github.com>
2024-10-28 20:15:43 +0300

    1.9.0

---
+++ src/main/java/net/weesli/rozsLib/RozsLib.java
@@ -0,0 +1,97 @@
+package net.weesli.rozsLib;
+
+import lombok.Getter;
+import net.weesli.rozsLib.bossbar.BossBarManager;
+import net.weesli.rozsLib.events.LibListener;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public final class RozsLib extends JavaPlugin {
+
+    private static final String GITHUB_API_URL = "https://api.github.com/repos/Weesli/RLib/releases";
+
+    @Getter private static RozsLib instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        Bukkit.getConsoleSender().sendMessage("[RLib] Starting plugin...");
+
+        // check lib version
+        Bukkit.getScheduler().runTask(this, ()-> {
+            if (!checkVersion()) {
+                Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[RLib] A new version of RLib is available. Please update!");
+            }else {
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "[RLib] You are using the latest version of RLib.");
+            }
+        });
+
+        // register lib events
+
+        this.getServer().getPluginManager().registerEvents(new LibListener(), this);
+
+    }
+
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("Stopping plugin...");
+        BossBarManager.getRegisteredBars().values().forEach(bossbar-> bossbar.removeAll());
+    }
+
+
+    private boolean checkVersion() {
+        try {
+            URL url = new URL(GITHUB_API_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                JSONParser parser = new JSONParser();
+                JSONArray releases = (JSONArray) parser.parse(response.toString());
+
+                if (!releases.isEmpty()) {
+                    JSONObject latestRelease = (JSONObject) releases.get(0);
+                    String tagName = latestRelease.get("tag_name").toString();
+                    if (tagName.equals(this.getDescription().getVersion())){
+                        return true;
+                    }else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } catch (IOException e) {
+            System.out.println("Error parsing");
+        }catch (ParseException e) {
+            System.out.println("Error parsing");
+        }
+        return false;
+    }
+
+}
