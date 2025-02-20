5dd8b6cdfd268ab51d3156bb7cda5bf7f9465c12
Weesli <78960827+Weesli@users.noreply.github.com>
2025-02-20 18:42:56 +0300

    Added auto create db

--- src/main/java/net/weesli/rozsLib/database/Database.java
+++ src/main/java/net/weesli/rozsLib/database/Database.java
@@ -13,9 +13,26 @@ public class Database {
     private ConnectionInfo info;
     private Connection connection;
 
+    @SneakyThrows
     public Database(ConnectionInfo info){
         this.info = info;
         this.connection = DatabaseFactory.createConnection(info);
+        if (info.getType().equals(DatabaseType.MySQL)){
+            String checkDbQuery = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?";
+            try (PreparedStatement stmt = getConnection().prepareStatement(checkDbQuery)) {
+                stmt.setString(1, info.getDbName());
+                ResultSet rs = stmt.executeQuery();
+
+                if (!rs.next()) {
+                    String createDbQuery = "CREATE DATABASE " + info.getDbName();
+                    try (Statement stmtCreate = getConnection().createStatement()) {
+                        stmtCreate.executeUpdate(createDbQuery);
+                        System.out.println("Database created: " + info.getDbName());
+                    }
+                }
+            }
+            this.connection = DriverManager.getConnection(info.getUrl() + info.getDbName(), info.getUsername(), info.getPassword());
+        }
     }
 
     public void close(){
--- src/main/java/net/weesli/rozsLib/database/DatabaseFactory.java
+++ src/main/java/net/weesli/rozsLib/database/DatabaseFactory.java
@@ -3,40 +3,16 @@ package net.weesli.rozsLib.database;
 import lombok.SneakyThrows;
 import net.weesli.rozsLib.enums.DatabaseType;
 
-import java.net.URI;
 import java.sql.*;
 
 public class DatabaseFactory {
 
     @SneakyThrows
     public static Connection createConnection(ConnectionInfo info){
-        try {
-            if (info.getType().equals(DatabaseType.SQLite)){
-                return DriverManager.getConnection(info.getUrl());
-            } else if (info.getType().equals(DatabaseType.MySQL)) {
-                return DriverManager.getConnection(info.getUrl(), info.getUsername(), info.getPassword());
-            }
-        }catch (SQLSyntaxErrorException e){
-            if (e.getMessage().contains("Unknown database")){
-                if (info.getType().equals(DatabaseType.MySQL)){
-                    try (Connection conn = DriverManager.getConnection(info.getUrl())) {
-                        String checkDbQuery = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?";
-                        try (PreparedStatement stmt = conn.prepareStatement(checkDbQuery)) {
-                            stmt.setString(1, info.getDbName());
-                            ResultSet rs = stmt.executeQuery();
-
-                            if (!rs.next()) {
-                                String createDbQuery = "CREATE DATABASE " + info.getDbName();
-                                try (Statement stmtCreate = conn.createStatement()) {
-                                    stmtCreate.executeUpdate(createDbQuery);
-                                    System.out.println("Database created: " + info.getDbName());
-                                }
-                            }
-                        }
-                    }
-                    return DriverManager.getConnection(info.getUrl(), info.getUsername(), info.getPassword());
-                }
-            }
+        if (info.getType().equals(DatabaseType.SQLite)){
+            return DriverManager.getConnection(info.getUrl());
+        } else if (info.getType().equals(DatabaseType.MySQL)) {
+            return DriverManager.getConnection(info.getUrl(), info.getUsername(), info.getPassword());
         }
         throw new IllegalArgumentException("Unsupported database type: " + info.getType());
     }
