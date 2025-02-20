700fbb10f3ccecce81b60cfc405e2e68b5157846
Weesli <78960827+Weesli@users.noreply.github.com>
2025-02-20 18:16:23 +0300

    Added auto create db

--- src/main/java/net/weesli/rozsLib/database/Database.java
+++ src/main/java/net/weesli/rozsLib/database/Database.java
@@ -13,29 +13,9 @@ public class Database {
     private ConnectionInfo info;
     private Connection connection;
 
-    @SneakyThrows
     public Database(ConnectionInfo info){
         this.info = info;
         this.connection = DatabaseFactory.createConnection(info);
-        if (info.getType().equals(DatabaseType.MySQL)){
-            URI uri = new URI(info.getUrl().replace("mysql://", "http://"));
-            String dbName = uri.getPath().substring(1);
-            try (Connection conn = DriverManager.getConnection(info.getUrl())) {
-                String checkDbQuery = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?";
-                try (PreparedStatement stmt = conn.prepareStatement(checkDbQuery)) {
-                    stmt.setString(1, dbName);
-                    ResultSet rs = stmt.executeQuery();
-
-                    if (!rs.next()) {
-                        String createDbQuery = "CREATE DATABASE " + dbName;
-                        try (Statement stmtCreate = conn.createStatement()) {
-                            stmtCreate.executeUpdate(createDbQuery);
-                            System.out.println("Database created: " + dbName);
-                        }
-                    }
-                }
-            }
-        }
     }
 
     public void close(){
--- src/main/java/net/weesli/rozsLib/database/DatabaseFactory.java
+++ src/main/java/net/weesli/rozsLib/database/DatabaseFactory.java
@@ -3,17 +3,41 @@ package net.weesli.rozsLib.database;
 import lombok.SneakyThrows;
 import net.weesli.rozsLib.enums.DatabaseType;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.net.URI;
+import java.sql.*;
 
 public class DatabaseFactory {
 
     @SneakyThrows
     public static Connection createConnection(ConnectionInfo info){
-        if (info.getType().equals(DatabaseType.SQLite)){
-            return DriverManager.getConnection(info.getUrl());
-        } else if (info.getType().equals(DatabaseType.MySQL)) {
-            return DriverManager.getConnection(info.getUrl(), info.getUsername(), info.getPassword());
+        try {
+            if (info.getType().equals(DatabaseType.SQLite)){
+                return DriverManager.getConnection(info.getUrl());
+            } else if (info.getType().equals(DatabaseType.MySQL)) {
+                return DriverManager.getConnection(info.getUrl(), info.getUsername(), info.getPassword());
+            }
+        }catch (SQLSyntaxErrorException e){
+            if (e.getMessage().contains("Unknown database")){
+                if (info.getType().equals(DatabaseType.MySQL)){
+                    URI uri = new URI(info.getUrl().replace("mysql://", "http://"));
+                    String dbName = uri.getPath().substring(1);
+                    try (Connection conn = DriverManager.getConnection(info.getUrl())) {
+                        String checkDbQuery = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?";
+                        try (PreparedStatement stmt = conn.prepareStatement(checkDbQuery)) {
+                            stmt.setString(1, dbName);
+                            ResultSet rs = stmt.executeQuery();
+
+                            if (!rs.next()) {
+                                String createDbQuery = "CREATE DATABASE " + dbName;
+                                try (Statement stmtCreate = conn.createStatement()) {
+                                    stmtCreate.executeUpdate(createDbQuery);
+                                    System.out.println("Database created: " + dbName);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
         throw new IllegalArgumentException("Unsupported database type: " + info.getType());
     }
