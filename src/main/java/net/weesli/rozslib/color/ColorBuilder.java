a629afe11fb22a72f01132325b0e9a1a5a55ae97
Weesl <sefasam553498@gmail.com>
2025-08-23 21:32:24 +0300

    disabled default italic message for minimessage

--- src/main/java/net/weesli/rozslib/color/ColorBuilder.java
+++ src/main/java/net/weesli/rozslib/color/ColorBuilder.java
@@ -2,16 +2,24 @@ package net.weesli.rozslib.color;
 
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class ColorBuilder {
 
-    public static Component convertColors(String input, TagResolver... resolvers) {
+    public static Component convertColors(String input, TagResolver... tagResolvers) {
         MiniMessage mm = MiniMessage.miniMessage();
-        Component legacy = LegacyComponentSerializer.legacyAmpersand().deserialize(input);
+
+        Component legacy = LegacyComponentSerializer.builder()
+                .hexColors()
+                .character('&')
+                .build()
+                .deserialize(input);
+
         String minimessage = mm.serialize(legacy).replace("\\", "");
-        return mm.deserialize(minimessage, resolvers);
+        return mm.deserialize(minimessage, tagResolvers).decoration(TextDecoration.ITALIC, false);
     }
+
 }
