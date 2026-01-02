86f936dd8e0405ed677d99e6bb50428ef5d5ae39
Weesl <sefasam553498@gmail.com>
2026-01-03 01:57:54 +0300

    database component deleted

--- src/main/java/net/weesli/rozslib/database/serializer/ObjectSerializer.java
+++
@@ -1,9 +0,0 @@
-package net.weesli.rozslib.database.serializer;
-
-public interface ObjectSerializer<T> {
-
-    String serialize(T object);
-    Object deserialize(String value);
-
-    boolean canSerialize(Class<?> clazz);
-}
