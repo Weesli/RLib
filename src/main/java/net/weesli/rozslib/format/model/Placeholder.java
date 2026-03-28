5e74c7eb431dc670da13461c0603ce62e8a48ba9
Weesli <sefasam553498@gmail.com>
2026-03-29 02:35:52 +0300

    added non-object version for placeholder

--- src/main/java/net/weesli/rozslib/format/model/Placeholder.java
+++ src/main/java/net/weesli/rozslib/format/model/Placeholder.java
@@ -1,24 +1,42 @@
 package net.weesli.rozslib.format.model;
 
 public record Placeholder(String name, String value) {
-
+    @Deprecated
     public Placeholder(String name, Integer value) {
         this(name, String.valueOf(value));
     }
-
+    @Deprecated
     public Placeholder(String name, Double value) {
         this(name, String.valueOf(value));
     }
-
+    @Deprecated
     public Placeholder(String name, Float value) {
         this(name, String.valueOf(value));
     }
-
+    @Deprecated
     public Placeholder(String name, Long value) {
         this(name, String.valueOf(value));
     }
-
+    @Deprecated
     public Placeholder(String name, Boolean value) {
         this(name, String.valueOf(value));
+    }
+
+    public static Placeholder of(String name, Integer value){
+        return new Placeholder(name, String.valueOf(value));
+    }
+
+    public static Placeholder of(String name, Double value){
+        return new Placeholder(name,String.valueOf(value));
+    }
+
+    public static Placeholder of(String name, Float value){
+        return new Placeholder(name,String.valueOf(value));
+    }
+    public static Placeholder of(String name, Long value){
+        return new Placeholder(name,String.valueOf(value));
+    }
+    public static Placeholder of(String name, Boolean value){
+        return new Placeholder(name,String.valueOf(value));
     }
 }
